32a03e65b7744819afcf3ab78cd6019d4849b457
Ihsan <ihsanriaz@gmail.com>
2018-09-07 02:01:08 +0100

    Blog functinality and Polish

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,16 @@ class Blog(models.Model):
     pub_date=models.DateTimeField(max_length=20)
     body=models.TextField(max_length=500)
     image=models.ImageField(upload_to='images/')
+
+#function to show title in the list of blogs
+
+    def __str__(self):
+        return self.title
+
+
+#function to reduce the size of the summary
+    def summary(self):
+        return self.body[:100]
+#function to fomrat the date Also use of the strftime from strftime.org
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
